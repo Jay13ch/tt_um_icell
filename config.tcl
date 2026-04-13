e500c6600e8abe19caba06632d209a5e90c95818
Jay13ch <jaya.ece@cambridge.edu.in>
2026-04-13 19:54:52 +0530

    Update design name and file paths in config.tcl

--- config.tcl
+++ config.tcl
@@ -1,13 +1,13 @@
-set ::env(DESIGN_NAME) tt_um_icell
-set ::env(VERILOG_FILES) "src/tt_um_icell.v"
+set ::env(DESIGN_NAME) tt_um_jaycam_icell
+set ::env(VERILOG_FILES) "src/tt_um_jaycam_icell.v"
 
 set ::env(CLOCK_PORT) ""
 set ::env(CLOCK_NET) ""
 
 set ::env(FP_CORE_UTIL) 5
 
-set ::env(EXTRA_LEFS) "analog/icell.lef"
-set ::env(EXTRA_GDS_FILES) "analog/icell.gds"
+set ::env(EXTRA_LEFS) "lef/icell.lef"
+set ::env(EXTRA_GDS_FILES) "gds/icell.gds"
 
 set ::env(VDD_NETS) "VPWR"
 set ::env(GND_NETS) "VGND"
